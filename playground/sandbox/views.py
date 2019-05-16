d8f1f7f355cf1f93d685a371e5cb0e3521b6fe4c
Yves Desjardins <YvesDesjardins@gmail.com>
2019-05-16 14:18:39 -0700

    implement rest framework and some models

--- playground/sandbox/views.py
+++ playground/sandbox/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('sandbox/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results for question {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
